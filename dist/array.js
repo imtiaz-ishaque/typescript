674e6e9e322bc4daef07177f0fe791be69bcb320
Imtiaz Baloch <imtiaz.mishaque@gmail.com>
2023-09-12 18:47:28 +0500

    TypeScript Arrays

---
+++ dist/array.js
@@ -0,0 +1,5 @@
+"use strict";
+const names = [];
+names.push("Imtiaz");
+console.log(names);
+//# sourceMappingURL=array.js.map
---
+++ src/array.ts
@@ -0,0 +1,11 @@
+/* 
+
+TypeScript Arrays
+TypeScript has a specific syntax for typing arrays.
+
+*/
+
+const names: string[] = [];
+names.push("Imtiaz"); //no error
+console.log(names);
+// names.push(100); //Error: Argument of type 'number' is not assignable to parameter of type 'string'.
