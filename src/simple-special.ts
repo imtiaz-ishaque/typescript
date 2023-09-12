5c800e201f9b51d4ce4f8423c9e6fedca1a29975
Imtiaz Baloch <imtiaz.mishaque@gmail.com>
2023-09-12 18:33:44 +0500

    Simple Special Type never

--- src/simple-special.ts
+++ src/simple-special.ts
@@ -59,3 +59,17 @@ unknown is best used when you don't know the type of data being typed. To add a 
 
 Casting is when we use the "as" keyword to say property or variable is of the casted type.
 */
+
+
+/* 
+Type: never
+never effectively throws an error whenever it is defined. 
+
+let x: never = true;  //Error: Type 'boolean' is not assignable to type 'never'.
+
+never is rarely used, especially by itself, its primary use is in advanced generics.
+*/
+
+
+
+
