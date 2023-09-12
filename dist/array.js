ea35533a9a752ed05b0e5ce14f6bf39ebc19210f
Imtiaz Baloch <imtiaz.mishaque@gmail.com>
2023-09-12 18:59:42 +0500

    TypeScript Array Type Inference

--- dist/array.js
+++ dist/array.js
@@ -2,4 +2,12 @@
 const names = [];
 names.push("Imtiaz");
 console.log(names);
+const names1 = ["Imtiaz"];
+console.log(names1);
+const numbers = [1, 2, 3];
+console.log(numbers);
+numbers.push(4);
+console.log(numbers);
+let head = numbers[0];
+console.log(head);
 //# sourceMappingURL=array.js.map
--- src/array.ts
+++ src/array.ts
@@ -19,3 +19,17 @@ const names1: readonly string[] = ["Imtiaz"];
 console.log(names1);
 // names1.push("Ahmed"); // Error: Property 'push' does not exist on type 'readonly string[]'.
 // try removing the readonly modifier and see if it works?
+
+/* 
+Type Inference
+TypeScript can infer the type of an array if it has values.
+*/
+
+const numbers = [1, 2, 3];
+console.log(numbers);
+numbers.push(4); //no error
+console.log(numbers);
+
+//numbers.push("2"); // Error: Argument of type 'string' is not assignable to parameter of type 'number'.
+let head: number = numbers[0];
+console.log(head);
